7e9078117e7cc9c2174c2e36e81ffdf9cf38de6d
Connor Kennelly <knelly26@gmail.com>
2016-04-18 19:41:11 -0700

    M. V. and C implemented.  Connecting next.

--- MvcMovie2/MvcMovie2/App_Start/RouteConfig.cs
+++ MvcMovie2/MvcMovie2/App_Start/RouteConfig.cs
@@ -18,6 +18,11 @@ namespace MvcMovie2
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Hello",
+                url: "{controller}/{action}/{name}/{id}"
+            );
         }
     }
 }
