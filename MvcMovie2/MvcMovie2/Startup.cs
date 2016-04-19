be07f3f40721541c0a02760635a75bb8322cdab3
Connor Kennelly <knelly26@gmail.com>
2016-04-18 18:52:16 -0700

    Restarted

---
+++ MvcMovie2/MvcMovie2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovie2.Startup))]
+namespace MvcMovie2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
